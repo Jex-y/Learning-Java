5993f08ce3b139b8e972dc08c1a677466b0f8eb1
Jex-y <edwardjex@live.co.uk>
2019-09-13 12:55:44 +0100

    Operators.java added
    Signed-off-by: Jex-y <edwardjex@live.co.uk>

--- Java/Learning Java/src/Operators.java
+++ Java/Learning Java/src/Operators.java
@@ -1,5 +1,83 @@
 public class Operators {
     public static void main(String[] args) {
+        /*
+         *   ----------------------------------------------------------------------------------------------------
+         *                                         Arithmetic Operators
+         *  ----------------------------------------------------------------------------------------------------
+         */
+
+        // Arithmetic operators are used to perform basic mathematical operations.
+
+
+        int int1 = 1;
+        int int2 = 2;
+        int result;
+
+        result = int1 + int2;
+        result = int1 - int2;
+        result = int1 * int2;
+        result = int1 / int2;
+        result = int1 % int2;
+        result ++;
+        result --;
+
+        /*
+         *   ----------------------------------------------------------------------------------------------------
+         *                                         Assignment Operators
+         *  ----------------------------------------------------------------------------------------------------
+         */
+
+        // Assignment operators can modify a variable whilst assigning it.
+
+        result = int1;
+        result += int1;
+        result -= int1;
+        result *= int1;
+        result /= int1;
+        result %= int1;
+        // Bitwise
+        result &= int1;
+        result |= int1;
+        result ^= int1;
+        result >>= int1;
+        result <<= int1;
+
+
+        /*
+         *   ----------------------------------------------------------------------------------------------------
+         *                                         Comparison Operators
+         *  ----------------------------------------------------------------------------------------------------
+         */
+
+        // Used to compare two values.
+
+        boolean bool;
+
+        bool = (int1 == int2);
+        bool = (int1 != int2);
+        bool = (int1 > int2);
+        bool = (int1 < int2);
+        bool = (int1 >= int2);
+        bool = (int1 >= int2);
+
+
+        /*
+         *   ----------------------------------------------------------------------------------------------------
+         *                                         Logical Operators
+         *  ----------------------------------------------------------------------------------------------------
+         */
+
+        bool = (true && false);
+        bool = (true || false);
+        bool = !true;
+
+
+
+
+
+
+
+
 
     }
 }
