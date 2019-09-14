40f77f5b074b4dd1a4020d31cc77108793c8b15d
14nd <55253404+14nd@users.noreply.github.com>
2019-09-14 15:27:28 +0100

    Add files via upload

---
+++ Comments.java
@@ -0,0 +1,12 @@
+public class Comments {
+    public static void main(String[] args) {
+        //This is a single line Comment//
+        /*This
+        is
+        a
+        multi
+        line
+        comment
+         */
+    }
+}
---
+++ Strings.java
@@ -0,0 +1,19 @@
+public class Strings {
+    public static void main(String[] args) {
+        String greeting = "Hello";
+
+        String txt = "Hello";
+        System.out.println(txt.length());
+
+        System.out.println(txt.toUpperCase());
+
+        String firstName = "John ";
+        String lastName = "Doe";
+        System.out.println(firstName + lastName);
+
+        System.out.println(firstName.concat(lastName));
+
+        String txt2 = "Hello Everybody";
+        System.out.println(txt2.indexOf("e"));
+    }
+}
