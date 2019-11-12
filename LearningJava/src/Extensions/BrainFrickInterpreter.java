258a3470a7de3317d9e7ce5678126482917963fb
Jex-y <edwardjex@live.co.uk>
2019-11-12 19:40:10 +0000

    Adapted Queue.java to include circular queues.

--- LearningJava/src/Extensions/BrainFrickInterpreter.java
+++ LearningJava/src/Extensions/BrainFrickInterpreter.java
@@ -1,7 +1,36 @@
 package Extensions;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class BrainFrickInterpreter {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("File Path: ");
+        //String path = input.nextLine();
+        String path = "helloWorld.bf";
+        String program = fileRead("F:\\code\\School\\LearningJava\\src\\Extensions\\" + path);
+        System.out.printf(program);
 
+    }
+    private static String fileRead(String inputFile){
+        char data = 0;
+        String outputData = "";
+        try {
+            FileReader reader = new FileReader(inputFile);
+            data = (char)reader.read();
+            while (data != (char)-1){
+                outputData += data; // Strip chars don't care about using cool regex
+                data = (char)reader.read();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return outputData;
     }
 }
--- LearningJava/src/Unit2/Notes/Queue.java
+++ LearningJava/src/Unit2/Notes/Queue.java
@@ -7,48 +7,102 @@ public class Queue<datatype> {
     private ArrayList<datatype> data;
     private int startPointer = 0;
     private int backPointer = 0;
-    private int size;
+    private int size = 0;
+    private int MAXSIZE;
     private String type;
 
-    Queue(int size) {
-        this(size, "Linear");
+    private final String [] validTypes = { "linear","circular" };
+
+    Queue(int MAXSIZE) {
+        this(MAXSIZE, "linear");
     }
 
-    Queue(int size, String type) {
-        this.data = new ArrayList<datatype>();
-        this.size = size;
+    Queue(int MAXSIZE, String type) {
+        boolean valid = false;
+        for (int i = 0; i < validTypes.length; i++) {
+            if (type.equals(validTypes[i])) {
+                valid = true;
+            }
+        }
+        if (!valid) {
+            System.err.println("Error: Invalid queue type, choose from: " + validTypes.toString());
+            System.exit(1);
+        }
+        data = new ArrayList<datatype>();
+        for (int i = 0; i < MAXSIZE; i++) { data.add(null); }
+        this.MAXSIZE = MAXSIZE;
         this.type = type;
     }
 
     public void put(datatype value){
-        if (backPointer < this.size) {
-            this.data.add(value);
-            backPointer++;
-        } else {
-            System.err.println("End of queue reached, cannot put more data");
+        if (type.equals("linear")) {
+            if (backPointer < MAXSIZE) {
+                data.add(value);
+                backPointer++;
+                size++;
+            } else {
+                System.err.println("Error: queue full, cannot put");
+            }
+        }
+        else if (type.equals("circular")) {
+            if (size < MAXSIZE) {
+                backPointer %= MAXSIZE;
+                data.set(backPointer,value);
+                backPointer ++;
+                size ++;
+            }
+            else {
+                System.err.println("Error: queue full, cannot put");
+            }
         }
     }
 
     public datatype pop(){
-        datatype result;
-        if (startPointer < backPointer) {
-            result = this.data.get(startPointer);
-            this.data.set(startPointer,null);
-            startPointer++;
-        } else {
-            result = null;
-            System.err.println("No more data left in queue to pop");
+        datatype result = null;
+        if (type.equals("linear")) {
+            if (startPointer < backPointer) {
+                result = data.get(startPointer);
+                data.set(startPointer, null);
+                startPointer++;
+                size--;
+            } else {
+                System.err.println("Error: Queue empty, cannot pop");
+            }
+        }
+        else if (type.equals("circular")) {
+            if (size != 0) {
+                startPointer %= MAXSIZE;
+                result = data.get(startPointer);
+                startPointer++;
+                size--;
+            } else {
+                System.err.println("Error: Queue empty, cannot pop");
+            }
         }
         return result;
     }
+
+    public datatype peek(){
+        datatype result = null;
+        if (size != 0) {
+            result = data.get(startPointer);
+        } else {
+            System.err.println("Error: Queue empty, cannot peek");
+        }
+        return result;
+    }
+
+    public boolean isFull(){ return size == MAXSIZE; }
+
+    public boolean isEmpty(){ return size == 0; }
 
     public String toString() {
         String output = "[ ";
         boolean first = true;
         datatype value;
-        for ( int i = 0; i < this.size; i++ ) {
+        for ( int i = 0; i < MAXSIZE; i++ ) {
             if ( i < backPointer ){
-                value = this.data.get(i);
+                value = data.get(i);
                 if (value != null) {
                     output += String.valueOf(value);
                 }
@@ -59,21 +113,26 @@ public class Queue<datatype> {
             else {
                 output += "#";
             }
-            if ( i != this.size - 1 ) { output += ", "; }
+            if ( i != MAXSIZE - 1 ) { output += ", "; }
         }
         output += " ]\nStart Pointer: ";
-        output += String.valueOf(this.startPointer);
+        output += String.valueOf(startPointer);
         output += "\tBack Pointer: ";
-        output += String.valueOf(this.backPointer);
+        output += String.valueOf(backPointer);
+        output += "\tMax Size: ";
+        output += String.valueOf(MAXSIZE);
+        output += "\tType: ";
+        output += type;
         return output;
     }
 
     public static void main(String[] args) {
-        Queue x = new Queue<Integer>(3);
+        Queue x = new Queue<Integer>(3,"circular");
         x.put(1);
         x.put(2);
         x.put(3);
         System.out.println(x.pop());
+        x.put(4);
         System.out.println(x.toString());
         System.out.println(x.pop());
         System.out.println(x.pop());
