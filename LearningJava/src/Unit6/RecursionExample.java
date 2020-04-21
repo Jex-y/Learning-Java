c9d31d84fb52f42443c821790d3c85668f73ca97
Edward Jex <edwardjex@live.co.uk>
2020-04-21 16:01:25 +0100

    Added RecursionExample

---
+++ LearningJava/src/Unit6/RecursionExample.java
@@ -0,0 +1,18 @@
+package Unit6;
+
+public class RecursionExample {
+    public static void main(String[] args) {
+        for (int i = 0; i < 101; i++) {
+            System.out.printf("The sum of all numbers from 0 to %d is %d\n",i,sum(i));
+        }
+    }
+
+    static int sum(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        else {
+            return num + sum(num-1);
+        }
+    }
+}
